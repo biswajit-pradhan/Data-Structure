05c03c01272fc75fbd5a82df3dc65f13574c2eb2
Biswajit Pradhan <pbiswajit967@gmail.com>
2022-02-16 09:32:20 +0530

    Data updated

--- Data Structure using python/Linked List/Single Linked List/linkedlist.py
+++ Data Structure using python/Linked List/Single Linked List/linkedlist.py
@@ -5,19 +5,32 @@ class node:
 class linkedlist:
     def __init__(self):
         self.head=None
+        
     def printll(self):
         if self.head is None:
             print("Linked List is empty.\n")
         else:
             n=self.head
             while n is not None:
-                print(n.data)
+                print(n.data,"-->",end=" ")
                 n=n.ref
+                
     def addbegin(self,data):
         new_node=node(data)
         new_node.ref=self.head
         self.head=new_node
         print("\nData inserted successfully\n")
+        
+    def addend(self,data):
+        new_node=node(data)
+        if self.head is None:
+            self.head=new_node
+        else:
+            n=self.head
+            while n.ref is not None:
+                n=n.ref
+            n.ref=new_node
+            
 if __name__=="__main__":
     ll1=linkedlist()
     while(True):
@@ -29,5 +42,8 @@ if __name__=="__main__":
             ll1.addbegin(value)
         elif key=='2':
             break
+        elif key=='5':
+            value=input("Enter data to the node: ")
+            ll1.addend(value)
         else:
             print("Enter correct option\n")
