c8a63025b8c3f9a286131a769669504e362f6b66
Biswajit Pradhan <pbiswajit967@gmail.com>
2022-02-22 23:13:59 +0530

    data updated

--- Data Structure using python/Linked List/Single Linked List/linkedlist.py
+++ Data Structure using python/Linked List/Single Linked List/linkedlist.py
@@ -1,4 +1,4 @@
-class node:
+class Node:
     def __init__(self,data):
         self.data=data
         self.ref=None
@@ -11,18 +11,20 @@ class linkedlist:
             print("Linked List is empty.\n")
         else:
             n=self.head
+            print("\n")
             while n is not None:
                 print(n.data,"-->",end=" ")
                 n=n.ref
+            print("\n")
                 
     def addbegin(self,data):
-        new_node=node(data)
+        new_node=Node(data)
         new_node.ref=self.head
         self.head=new_node
         print("\nData inserted successfully\n")
         
     def addend(self,data):
-        new_node=node(data)
+        new_node=Node(data)
         if self.head is None:
             self.head=new_node
         else:
@@ -30,11 +32,25 @@ class linkedlist:
             while n.ref is not None:
                 n=n.ref
             n.ref=new_node
+        print("\nData inserted successfully\n")
+    def add_after(self,data,x):
+        n=self.head
+        while n is not None:
+            if x==n.data:
+                break;
+            n=n.ref
+        if n is None:
+            print("Node is not present in the Linked List\n")
+        else:
+            new_node=Node(data)
+            new_node.ref=n.ref
+            n.ref=new_node
+            print("\nData inserted successfully\n")
             
 if __name__=="__main__":
     ll1=linkedlist()
     while(True):
-        key=input("\n1.Traverse the linkedlist\n2.Quit\n3.Insert a node at begin\n4.Insert a node in between\n5.Insert a node at the end\n\nChoose an option: ")
+        key=input("\n1.Traverse the linkedlist\n2.Quit\n3.Insert a node at begin\n4.Insert after a node\n5.Insert a node at the end\n\nChoose an option: ")
         if key=='1':
             ll1.printll()
         elif key=='3':
@@ -45,5 +61,9 @@ if __name__=="__main__":
         elif key=='5':
             value=input("Enter data to the node: ")
             ll1.addend(value)
+        elif key=='4':
+            value=int(input("Enter data to the node: "))
+            x=int(input("Enter the node value after which data to be inserted: "))
+            ll1.add_after(value,x)
         else:
             print("Enter correct option\n")
